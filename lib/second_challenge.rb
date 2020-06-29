215e9fff5a1f50a40e77df58cc248c3e36b0fb88
Idalis Paz <idalispaz@gmail.com>
2020-06-29 04:58:19 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,12 +1,19 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
       
       }, 
     :capulet => {
+      :partriarch => {},
+      :matriarch => {},
+      :herione => {},
+      :herione_friends => []
      
       }
-    
   }
 
   
